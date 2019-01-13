75c8a0ea33ec12df39cdf7d7d2d5a1bc6c16c63c
slientbear <tianqo@sohu.com>
2019-01-13 11:58:31 +0800

    还没完成表单验证

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import './assets/base.css';
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
